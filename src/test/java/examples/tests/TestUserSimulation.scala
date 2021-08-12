36a8dc449da6b85834ff1932268642f84cb2bb09
sadhaw <88412834+sadhaw@users.noreply.github.com>
2021-08-12 15:01:48 +1000

    Update TestUserSimulation.scala

--- src/test/java/examples/tests/TestUserSimulation.scala
+++ src/test/java/examples/tests/TestUserSimulation.scala
@@ -7,10 +7,20 @@ import scala.language.postfixOps
 
 class TestUserSimulation extends Simulation {
 
+  before {
+    println("Tasklist Management Load Tests are about to start!!")
+  }
+  val rampUsersCount: Int = Properties.propOrElse("rampUsers", "10").toInt
+  val rampDuration: Int = Properties.propOrElse("rampDuration", "5").toInt
   {
     val getUser = scenario("getCall").exec(karateFeature("classpath:examples/tests/CURD.feature"))
 
     setUp(
-      getUser.inject(rampUsers(10) during (5 seconds)))
+      // getUser.inject(rampUsers(10) during (5 seconds)))
+      getUser.inject(rampUsers(rampUsersCount) during rampDuration)
+  }
+
+    after {
+    println("Tasklist Management Load Tests finished!!")
   }
 }
